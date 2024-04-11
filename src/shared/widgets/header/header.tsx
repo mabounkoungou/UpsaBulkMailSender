9920f458a8a498cf5a76f304f3a2bdd189063e2e
Ghostwriter <mabounkoungou001@st.ug.edu.gh>
2024-04-11 23:59:21 +0000

    update

--- src/shared/widgets/header/header.tsx
+++ src/shared/widgets/header/header.tsx
@@ -6,7 +6,7 @@ import Toolbar from './toolbar'
 const Header = () => {
   return (
    <header className='w-full sticky top-0 left-0 z-[999] border-b border-b[#22d3ee] px-10 flex items-center justify-between h-[80px] bg-white text-black'>
-      <div>
+      <div className='[#22d3ee]'>
         <Link href={"/"}>
           <Logo />
         </Link>
