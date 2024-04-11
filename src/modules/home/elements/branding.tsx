7b2c166e4f2196ae5c5f2166131672269b5bebaf
Ghostwriter <mabounkoungou001@st.ug.edu.gh>
2024-04-11 23:47:58 +0000

    update

--- src/modules/home/elements/branding.tsx
+++ src/modules/home/elements/branding.tsx
@@ -6,10 +6,10 @@ const Branding = () => {
   return (
     <div className="border-t border-b border-[#000] py-10">
       <h3 className="uppercase text-xl md:text-2xl text-center max-w-3xl mx-auto font-[400] z-20 relative">
-        CREATED BY THE EARLY MORNING BREW TEAM
+        CREATED BY THE ASMIC TEAM
       </h3>
       <div className="w-full text-center pt-1">
-        <h3 className="uppercase bg-[#F091DD] rounded p-2 text-xl md:text-2xl text-center inline-block font-medium z-20 relative">
+        <h3 className="uppercase bg-[#22d3ee] rounded p-2 text-xl md:text-2xl text-center inline-block font-medium z-20 relative">
           NOW POWERING THE WORLD&apos;S TOP NEWSLETTERS
         </h3>
       </div>
