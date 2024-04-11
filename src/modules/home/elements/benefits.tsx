a9f59331660dcf4528edb22feff2859f8416ea72
Ghostwriter <mabounkoungou001@st.ug.edu.gh>
2024-04-11 23:58:01 +0000

    update

--- src/modules/home/elements/benefits.tsx
+++ src/modules/home/elements/benefits.tsx
@@ -2,7 +2,7 @@
 const Benefits = () => {
   return (
     <div className="benefit-cover min-h-[60vh] relative flex items-center justify-center">
-      <div className="w-[70%] md:h-[350px] bg-[#F7F5FF] border-[2px] rounded border-blue-500 p-2">
+      <div className="w-[70%] md:h-[350px] bg-[#22d3ee] border-[2px] rounded border-blue-500 p-2">
         <h3 className="font-clashDisplay uppercase text-3xl md:text-7xl text-center w-full mb-4">
           EVERYTHING YOU NEED TO <span className="font-style">SUCCEED</span>{" "}
           AVAILABLE IN A SINGLE PLATFORM
