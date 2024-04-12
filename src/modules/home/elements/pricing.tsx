1e8cd7bec106ac5ab8919eb6e9b210e9ebbdd756
Ghostwriter <mabounkoungou001@st.ug.edu.gh>
2024-04-12 00:01:09 +0000

    update

--- src/modules/home/elements/pricing.tsx
+++ src/modules/home/elements/pricing.tsx
@@ -7,7 +7,7 @@ import { useState } from "react";
 const Pricing = () => {
   const [active, setActive] = useState("Monthly");
   return (
-    <div className="w-full bg-[#fec8eb]">
+    <div className="w-full bg-[#22d3ee]">
       <div className="w-[95%] m-auto py-5">
         <div className="w-full md:flex justify-between">
           <div>
