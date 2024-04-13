8e166a2d6e1ecddb9059357f5abb695a9f6181ee
Ghostwriter <mabounkoungou001@st.ug.edu.gh>
2024-04-13 03:35:30 +0000

    update

--- src/models/email.model.tsx
+++ src/models/email.model.tsx
@@ -17,5 +17,5 @@ const emailSchema = new Schema(
   { timestamps: true }
 );
 
-const Email = mongoose.models.Emails || mongoose.model("Emails", emailSchema);
+const Email = mongoose.models.Email || mongoose.model("Email", emailSchema);
 export default Email;
