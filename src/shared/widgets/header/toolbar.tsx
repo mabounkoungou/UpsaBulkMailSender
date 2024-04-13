38ced08e5df48b848bf93c9b4977c9bfa970f527
Ghostwriter <mabounkoungou001@st.ug.edu.gh>
2024-04-13 11:21:56 +0000

    login

--- src/shared/widgets/header/toolbar.tsx
+++ src/shared/widgets/header/toolbar.tsx
@@ -24,7 +24,7 @@ const Toolbar = () => {
           </Link>
         </>
       ) : (
-        <Link href={"/sign-in"}>Login</Link>
+        <Link href={"/sign-in"}><Button className="btn btn-primary">Signin</Button></Link>
       )}
     </>
   );
